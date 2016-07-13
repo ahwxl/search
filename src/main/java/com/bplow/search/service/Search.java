e4c42f935b033f1290ff3f07b4ddb7e9b3437914
wangxiaolei <wangxiaolei@shfft.com>
2016-07-13 09:34:21 +0800

    单元测试

--- src/main/java/com/bplow/search/service/Search.java
+++ src/main/java/com/bplow/search/service/Search.java
@@ -14,17 +14,30 @@ import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.highlight.Formatter;
+import org.apache.lucene.search.highlight.Fragmenter;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.Scorer;
+import org.apache.lucene.search.highlight.SimpleFragmenter;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
 
@@ -35,14 +48,23 @@ import org.springframework.stereotype.Service;
  * @version $Id: Search.java, v 0.1 2016年3月18日 上午10:10:14 wangxiaolei Exp $
  */
 @Service
-public class Search {
+public class Search implements InitializingBean{
+    
+    IndexSearcher isearcher = null;
+    
+    SmartChineseAnalyzer cnanalyzer = new SmartChineseAnalyzer();
     
     private Logger log = LoggerFactory.getLogger(this.getClass());
+    
+    public void afterPropertiesSet() throws Exception {
+        Path path = FileSystems.getDefault().getPath("D:/www/index");
+        Directory directory = FSDirectory.open(path);
+        DirectoryReader ireader = DirectoryReader.open(directory);
+        isearcher = new IndexSearcher(ireader);
+    }
 
     public void search() throws Exception {
         //Analyzer analyzer = new StandardAnalyzer();
-        SmartChineseAnalyzer cnanalyzer = new SmartChineseAnalyzer();
-
         // Store the index in memory:
         //Directory directory = new RAMDirectory();
         // To store an index on disk, use this instead:
@@ -79,7 +101,12 @@ public class Search {
         
     }
     
-    
+    /**
+     * 加索引
+     * 
+     * @param doc
+     * @throws IOException
+     */
     public void addDocToIndex(Document doc) throws IOException{
         SmartChineseAnalyzer cnanalyzer = new SmartChineseAnalyzer();
         Path path = FileSystems.getDefault().getPath("D:/www/index");
@@ -91,17 +118,122 @@ public class Search {
         iwriter.close();
     }
     
-    public ScoreDoc[] search(String querystr) throws IOException, ParseException{
-        SmartChineseAnalyzer cnanalyzer = new SmartChineseAnalyzer();
-        Path path = FileSystems.getDefault().getPath("D:/www/index");
-        Directory directory = FSDirectory.open(path);
-        DirectoryReader ireader = DirectoryReader.open(directory);
-        IndexSearcher isearcher = new IndexSearcher(ireader);
+    /*
+     *高亮
+     */
+    public ScoreDoc[] search(String fieldName,String querystr) throws IOException, ParseException, InvalidTokenOffsetsException{
+        Long starttime = System.currentTimeMillis();
         
-        QueryParser parser = new QueryParser("fieldname", cnanalyzer);
-        Query query = parser.parse("篮球");
+        QueryParser parser = new QueryParser(fieldName, cnanalyzer);
+        Query query = parser.parse(querystr);
+        
+        Formatter formatter = new SimpleHTMLFormatter(  
+            "<span class=\"highlighter\">", "</span>");
+        Scorer fragmentScorer = new QueryScorer(query);
+        Highlighter highlighter = new Highlighter(formatter, fragmentScorer);  
+        Fragmenter fragmenter = new SimpleFragmenter(100);// 高亮范围  
+        highlighter.setTextFragmenter(fragmenter);
+        
+        
         ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
+        log.info("耗时:{}",System.currentTimeMillis() - starttime);
+        for (int i = 0; i < hits.length; i++) {
+            Document hitDoc = isearcher.doc(hits[i].doc);
+            
+          //如果当前属性值中没有出现关键字,则返回null  
+            String hctemp = highlighter.getBestFragment(
+                cnanalyzer, "content", hitDoc.get("content"));
+            if(null != hctemp){
+                log.info("高亮:{}",hctemp);
+            }
+            
+            log.info(hitDoc.get("content"));
+            log.info("id:{}",hitDoc.get("id"));
+        }
         return hits;
     }
+    
+    /**
+     * 分页
+     * @throws IOException 
+     * @throws ParseException 
+     */
+    public void searchPage(String fieldName,String querystr) throws IOException, ParseException{
+        QueryParser parser = new QueryParser(fieldName, cnanalyzer);
+        Query query = parser.parse(querystr);
+        
+        int pageStart=0;
+        ScoreDoc lastBottom=null;//相当于pageSize
+        while(pageStart<60){//这个只有是paged.scoreDocs.length的倍数加一才有可能翻页操作
+            TopDocs paged=null;
+            paged=isearcher.searchAfter(lastBottom, query, null, 10);//查询首次的30条
+            if(paged.scoreDocs.length==0){
+                break;//如果下一页的命中数为0的情况下，循环自动结束
+            }
+            page(isearcher,paged);//分页操作，此步是传到方法里对数据做处理的
+
+            pageStart+=paged.scoreDocs.length;//下一次分页总在上一次分页的基础上
+            lastBottom=paged.scoreDocs[paged.scoreDocs.length-1];//上一次的总量-1，成为下一次的lastBottom
+        }
+    }
+
+
+    /*打印*/
+    private void page(IndexSearcher isearcher, TopDocs paged) throws IOException {
+        ScoreDoc[] hits = paged.scoreDocs;
+        for(int i = 0; i < hits.length; i++){
+            Document hitDoc = isearcher.doc(hits[i].doc);
+            log.info("记录[{},{}]",hitDoc.get("id"),hitDoc.get("content"));
+        }
+    }
+    
+    /**
+     * 关联查询
+     * 多条件查询
+     * @throws ParseException 
+     */
+    public void multiQuery(String keyword) throws ParseException{
+        String[] fields = { "phoneType", "name", "category", "price" };
+        Query query = new MultiFieldQueryParser(fields, cnanalyzer).parse(keyword);
+        
+        
+    }
+
+    
+    /**
+     * 排序
+     * @throws ParseException 
+     */
+    public void searchOrderBy(String fieldName,String keyword) throws ParseException{
+        String[] fields = { "content", "name", "category", "price" };
+        Query query = new MultiFieldQueryParser(fields, cnanalyzer).parse(keyword);
+        
+        
+        
+    }
+    
+    
+    /**
+     * 过滤
+     */
+    
+    
+    /**
+     * 结果合并
+     * 
+     */
+    
+
+    
+    
+    
+    
+    /**
+     * 加权
+     */
+    
+    /**
+     * 检索结果的评分
+     */
 
 }
--- src/test/java/com/bplow/search/service/SearchTest.java
+++ src/test/java/com/bplow/search/service/SearchTest.java
@@ -9,8 +9,14 @@ import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.LongField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -22,38 +28,85 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 public class SearchTest {
     
+    private Logger log = LoggerFactory.getLogger(SearchTest.class);
+    
     @Autowired
     private Search search;
     
+    @Ignore
     @Test
     public void searchTest() throws Exception{
         search.search();
         
     }
     
-    
+    /**
+     *测试日期 
+     */
+    @Ignore
+    @Test
     public void addDocTest() throws IOException{
-        /*id*/
-        FieldType fieldType = new FieldType();
-        fieldType.setStored(true);
-        fieldType.setTokenized(false);
-        Field field = new Field("id","1234567890",fieldType);
-        /*日期*/
-        Date date = new Date();
-        LongField longField = new LongField("date",date.getTime(),LongField.TYPE_STORED);
+        for(int i =0;i<30;i++){
+            /*id*/
+            FieldType fieldType = new FieldType();
+            fieldType.setStored(true);
+            fieldType.setTokenized(false);
+            Field field = new Field("id","A"+i,fieldType);
+            /*日期*/
+            Date date = new Date();
+            LongField longField = new LongField("date",date.getTime(),LongField.TYPE_STORED);
+            
+            /*url*/
+            Field stringField = new Field("url","http://agc.com",StringField.TYPE_STORED);
+            
+            /*content*/
+            Field textField = new Field("content","在水一方，大的撒飒飒的第三方的订单达到了",TextField.TYPE_STORED);
+            
+            Document doc = new Document();
+            doc.add(field);
+            doc.add(longField);
+            doc.add(stringField);
+            doc.add(textField);
+            
+            search.addDocToIndex(doc);
+        }
         
-        /*url*/
-        Field stringField = new Field("url","http://agc.com",StringField.TYPE_STORED);
+    }
+    
+    //@Ignore
+    //@Test
+    public void searchDate() throws IOException, ParseException, InvalidTokenOffsetsException{
+        ScoreDoc[] hits = search.search("content", "订单");
+        /*for (int i = 0; i < hits.length; i++) {
+            Document hitDoc = isearcher.doc(hits[i].doc);
+            log.info(hitDoc.get("fieldname"));
+        }*/
+    }
+    
+    /**
+     * 范围测试
+     * 
+     * @throws IOException
+     * @throws ParseException
+     * @throws InvalidTokenOffsetsException 
+     */
+    //@Ignore
+    //@Test
+    public void searchRange() throws IOException, ParseException, InvalidTokenOffsetsException{
+        ScoreDoc[] hits = search.search("content", "订单");
+    }
+    
+    /**
+     * 分页测试
+     * 
+     * @throws IOException
+     * @throws ParseException
+     */
+    //@Ignore
+    @Test
+    public void searchPage() throws IOException, ParseException{
+        search.searchPage("content", "订单");
         
-        Field textField = new Field("content","在水一方，大的撒飒飒的第三方的订单达到了",TextField.TYPE_STORED);
-        
-        Document doc = new Document();
-        doc.add(field);
-        doc.add(longField);
-        doc.add(stringField);
-        doc.add(textField);
-        
-        search.addDocToIndex(doc);
     }
     
     
