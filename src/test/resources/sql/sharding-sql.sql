4346e1c9a11a88670dbbc0cb097e8e7e88c4df0b
wangxiaolei <wangxiaolei@shfft.com>
2016-12-23 17:50:07 +0800

    初始sql

---
+++ src/test/resources/sql/sharding-sql.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `t_order_0` (
+	`order_id` INT(11) NOT NULL,
+	`order_name` VARCHAR(200) NULL DEFAULT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_1` (
+	`order_id` INT(11) NOT NULL,
+	`order_name` VARCHAR(200) NULL DEFAULT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_2` (
+	`order_id` INT(11) NOT NULL,
+	`order_name` VARCHAR(200) NULL DEFAULT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_3` (
+	`order_id` INT(11) NOT NULL,
+	`order_name` VARCHAR(200) NULL DEFAULT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+truncate table t_order_0;
+truncate table t_order_1;
+truncate table t_order_2;
+truncate table t_order_3;
